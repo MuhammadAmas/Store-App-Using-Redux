f22165bac306cae74a7ffb61e48e59e44ef552df
amas.waseemclear <amas.waseem@venturedive.com>
2023-04-10 20:31:24 +0500

    header changed

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return <>
         <div className="ui fixed menu">
             <div className="ui container center">
-                <h1>My App</h1>
+                <h1>Store App</h1>
             </div>
         </div>
     </>;
