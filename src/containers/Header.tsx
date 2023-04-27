f1950ca1422fb2c28757c8a6212c0bbea43d4058
amas.waseemclear <amas.waseem@venturedive.com>
2023-04-27 18:57:43 +0500

    updated

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-const Header = () => {
-
-    return <>
+const Header: React.FC = () => (
+  <>
         <div className="ui fixed menu">
             <div className="ui container center">
                 <h1>Store App</h1>
             </div>
         </div>
-    </>;
-}
+    </>
+);
 
 export default Header;
