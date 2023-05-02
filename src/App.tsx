f8c9fdd26f7f73cbb1fada445e2c1bed0964e1de
muhammadamas <amaswaseem@gmail.com>
2023-05-02 15:24:43 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import Header from './containers/Header'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductListing from './containers/ProductListing'
